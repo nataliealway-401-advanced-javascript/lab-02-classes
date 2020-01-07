f7ca49f12c55608d2aa39f45b570a406d8c5fc4b
natalielyn <nlmaxwell@gmail.com>
2020-01-07 14:06:16 -0800

    constructors to classes

--- classes/vehicle-class.js
+++ classes/vehicle-class.js
@@ -1,6 +1,25 @@
 'use strict';
 
-class Car {
+class Vehicle {
+    constructor(name, wheels) {
+        this.name = name;
+        this.wheels = wheels;
+ }
+ drive(){
+     return 'Moving Forward';
+ }
+ stop(){
+     return 'Stopping';
+ }
 }
 
+class Car extends Vehicle {
+    
+}
+
+class Motorcycle extends Vehicle {
+    
+}
+
+module.exports = Motorcycle;
 module.exports = Car;
