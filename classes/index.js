35ced9d57a0c7858a91759231320eb25e763fdc3
natalielyn <nlmaxwell@gmail.com>
2020-01-15 22:54:41 -0800

    added jsdoc

--- classes/index.js
+++ classes/index.js
@@ -2,14 +2,28 @@
 
 const VehicleConstructor = require('./vehicle-constructor.js');
 const VehicleClass = require('./vehicle-class.js');
-// Implement a car and motorcycle using a Constructor
+
+/**
+ * Implement a car using a Constructor
+ * @function
+ * @param {string} Assigns name to new instance of that vehicle
+ */
 const mazda = new VehicleConstructor.Car('Mazda 3');
 console.log(mazda.name, mazda.drive(), mazda.stop());
 
+/**
+ * Implement a motorcycle using a Constructor
+ * @function
+ * @param {string} Assigns name to new instance of that vehicle
+ */
 const harley = new VehicleConstructor.Motorcycle('Harley');
 console.log(harley.name, harley.wheelie(), harley.stop());
 
-// Implement a car and motorcycle using a Class
+/**
+ * Implement a car and motorcycle using a Class
+ * @function
+ * @param {string} Assigns name to new instance of both vehicle
+ */
 const subaru = new VehicleClass.Car('Subaru Legacy');
 console.log(subaru.name, subaru.drive(), subaru.stop());
 
--- validator/validator.js
+++ validator/validator.js
@@ -1,5 +1,17 @@
 'use strict';
+/**
+ * Based on a set of rules, is the input valid?
+ * TODO: Define the rules ... how do we send them in? How do we identify?
+ * @param input
+ * @param rules
+ * @returns {boolean}
+ */
 
+
+ /**
+  * Validator Function
+  * @function
+  */
 let validator = {};
 
 validator.isValid = (schema,data) => {
@@ -24,35 +36,75 @@ validator.isValid = (schema,data) => {
   return valid;
 };
 
-
+/**
+ * @function isString
+ * @param {string} 
+ * @returns {boolean}
+ */
 validator.isString = (input) => {
   return typeof input === 'string';
 };
 
+/**
+ * @function isNumber
+ * @param {Object} 
+ * @returns {boolean}
+ */
 validator.isNumber = (input) => {
   return typeof input === 'number';
 };
 
+/**
+ * @function isArray
+ * @param {array} valueType
+ * @returns {boolean}
+ */
 validator.isArray = (input, valueType) => {
   return Array.isArray(input) && (valueType? input.every(val => typeof val === valueType): true);
 };
 
+/**
+ * @function isObject
+ * @param {object} 
+ * @returns {boolean}
+ */
 validator.isObject = (input) => {
   return typeof input === 'object' && !(input instanceof Array);
 };
 
+/**
+ * @function isFunction
+ * @param {object} 
+ * @returns {boolean}
+ */
 validator.isFunction = (input) => {
   return typeof input === 'function';
 };
 
+/**
+ * @function isBoolean
+ * @param {object} 
+ * @returns {boolean}
+ */
 validator.isBoolean = (input) => {
   return typeof input === 'boolean';
 };
 
+/**
+ * @function isString
+ * @param {object} 
+ * @returns {boolean}
+ */
 validator.isTruthy = (input) => {
   return !!input;
 };
 
+/**
+ * @function isCorrectType
+ * @param input
+ * @param field
+ * @returns {boolean}
+ */
 validator.isCorrectType = (input, field) => {
   switch(field.type){
   case 'string': return validator.isString(input);
@@ -64,5 +116,9 @@ validator.isCorrectType = (input, field) => {
   }
 };
 
+/**
+ * Exports new validator instance
+ * @module Validator
+ */
 module.exports = validator;
 
