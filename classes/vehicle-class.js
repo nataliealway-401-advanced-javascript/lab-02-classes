a7ad49f8333b89d43c3e6e20f17662f0d2b669d1
natalielyn <nlmaxwell@gmail.com>
2020-01-16 16:16:32 -0800

    merge

--- classes/vehicle-class.js
+++ classes/vehicle-class.js
@@ -1,24 +1,43 @@
 'use strict';
-
+/**
+ * @class
+ * @param  {string} name: name of the vehicle
+ * @param  {number} wheels: number of wheels on the vehicle
+ */
 class Vehicle {
   constructor(name, wheels) {
     this.name = name;
     this.wheels = wheels;
   }
+  /**
+   * @function drive
+   * @returns {string}
+   */
   drive(){
     return 'Moving Forward';
   }
+  /**
+   * @function stop
+   * @returns {string}
+   */
   stop(){
     return 'Stopping';
   }
 }
-
+/**
+   * @class Car
+   * @param {string} name: Inherits name of parent Class
+   */
 class Car extends Vehicle {
   constructor(name) {
     super(name, 4);
   } 
 }
 
+/**
+   * @class Motorcycle
+   * @param {string} name: Inherits name of parent Class
+   */
 class Motorcycle extends Vehicle {
   constructor(name){
     super(name, 2);
@@ -27,5 +46,8 @@ class Motorcycle extends Vehicle {
     return 'Wheee!';
   }
 }
-
+/**
+   * Exports Car and Motorcycle objects
+   * @module Car/Motorcycle
+   */
 module.exports = {Car, Motorcycle};
--- classes/vehicle-constructor.js
+++ classes/vehicle-constructor.js
@@ -1,33 +1,69 @@
 'use strict';
-
+/**
+ * Vehicle Constructor
+ * @param  {} name
+ * @param  {} wheels
+ */
 const Vehicle = function(name, wheels) {
   this.name = name;
   this.wheels = wheels;
 };
 
+/**
+ * @function
+ * @returns {string}
+ */
 Vehicle.prototype.drive = () => {
   return 'Moving Forward';
 };
 
+/**
+ * @function
+ * @returns {string}
+ */
 Vehicle.prototype.stop = () => {
   return 'Stopping';
 };
 
-// Car Constructor
+
+/**
+ * Car Constructor
+ * @param  {} name
+ */
 const Car = function(name) {
   Vehicle.call(this, name, 4);
 };
 
+/**
+ * @function
+ * @returns  {object} 
+ */
 Car.prototype = new Vehicle();
 
+/**
+ * Motorcycle Constructor
+ * @param  {} name
+ */
 const Motorcycle = function(name) {
   Vehicle.call(this,name,2);
 };
 
+/**
+ * @function
+ * @returns  {object} 
+ */
 Motorcycle.prototype = new Vehicle();
 
+/**
+ * @function
+ * @returns  {string} 
+ */
 Motorcycle.prototype.wheelie = () => {
   return 'Wheee!';
 };
 
+/**
+   * Exports Car and Motorcycle objects
+   * @module Car/Motorcycle
+   */
 module.exports = {Car, Motorcycle};
